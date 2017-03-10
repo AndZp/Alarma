fca4e46da0963f7fbe118475128abc079e079494
Des63rus <shadowspan63rus@gmail.com>
2017-03-10 00:47:39 +0000

    Weather manager refactoring

--- app/src/main/java/mobi/mateam/alarma/weather/WeatherManager.java
+++ app/src/main/java/mobi/mateam/alarma/weather/WeatherManager.java
@@ -1,17 +1,28 @@
 package mobi.mateam.alarma.weather;
 
 
+import java.util.Map;
+
 import mobi.mateam.alarma.weather.model.AlarmWeatherConditions;
+import mobi.mateam.alarma.weather.model.CurrentWeatherState;
+import mobi.mateam.alarma.weather.model.ParameterType;
 import mobi.mateam.alarma.weather.model.WeatherCheckResponse;
 import mobi.mateam.alarma.weather.model.WeatherData;
-import rx.Observable;
+import mobi.mateam.alarma.weather.model.params.ProblemParam;
 
-public interface WeatherManager {
+public class WeatherManager {
 
     /**
-     * @param currentWeather
+     * @param currentWeatherData
+     *      current weather data in API format
      * @param alarmWeatherConditions
+     *      alarm weather conditions
      * @return
+     *      Weather check response - contains data isSutable and map of problems if not sutable
      */
-    public Observable<WeatherCheckResponse> checkTheWeather(WeatherData currentWeather, AlarmWeatherConditions alarmWeatherConditions);
+    public static WeatherCheckResponse checkTheWeather(WeatherData currentWeatherData, AlarmWeatherConditions alarmWeatherConditions){
+        CurrentWeatherState currentWeatherState = new CurrentWeatherState(currentWeatherData);
+        Map<ParameterType, ProblemParam> problems = alarmWeatherConditions.checkWeather(currentWeatherState);
+        return new WeatherCheckResponse(problems);
+    }
 }
---
+++ app/src/main/java/mobi/mateam/alarma/weather/model/params/ProblemParam.java
@@ -0,0 +1,15 @@
+package mobi.mateam.alarma.weather.model.params;
+
+/**
+ * Created by Des63rus on 3/10/2017.
+ *
+ * Class contains problematic value and range to showe user why alarm didnt rang
+ */
+
+public class ProblemParam{
+
+    private WeatherParamValue currentValue;
+
+    private WeatherParamRange range;
+
+}
