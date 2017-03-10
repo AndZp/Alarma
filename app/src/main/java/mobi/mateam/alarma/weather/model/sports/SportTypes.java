38e03211eed3e057d7ce21b9d30e33fd87eb7673
Des63rus <shadowspan63rus@gmail.com>
2017-03-10 05:51:19 +0000

    added comment

--- app/src/main/java/mobi/mateam/alarma/weather/model/sports/SportTypes.java
+++ app/src/main/java/mobi/mateam/alarma/weather/model/sports/SportTypes.java
@@ -13,20 +13,30 @@ import mobi.mateam.alarma.weather.model.ParameterType;
 
 public enum SportTypes {
     //TODO: move to strings all text values
-    RUNNING("Jogging", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
-    CYCLING("Cycling", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
-    SNOWBOARDING("Snowboarding", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
-    OTHERS("Others", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER);
+    RUNNING("Jogging",0, ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    CYCLING("Cycling",1, ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    SNOWBOARDING("Snowboarding", 2, ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    OTHERS("Others", 3, ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER);
 
     private String text;
 
     private List<ParameterType> defaultParams;
 
-    SportTypes(String text, ParameterType... params) {
+    private int imageId;
+
+    SportTypes(String text,int imageId,  ParameterType... params) {
         this.text = text;
         if (params == null && params.length == 0) {
             this.defaultParams = Arrays.asList();
         }
         this.defaultParams = Arrays.asList(params);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getImageId() {
+        return imageId;
     }
 }
