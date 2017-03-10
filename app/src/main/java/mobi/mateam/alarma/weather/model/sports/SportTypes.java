c184a642c3846334de9c5d08806f1865cf88b1dd
Des63rus <shadowspan63rus@gmail.com>
2017-03-10 05:31:56 +0000

    Default values

---
+++ app/src/main/java/mobi/mateam/alarma/weather/model/sports/SportTypes.java
@@ -0,0 +1,31 @@
+package mobi.mateam.alarma.weather.model.sports;
+
+import java.util.Arrays;
+import java.util.List;
+
+import mobi.mateam.alarma.weather.model.ParameterType;
+
+/**
+ * Created by Des63rus on 3/10/2017.
+ *
+ * Enum for sport types with default params
+ */
+
+public enum SportTypes {
+    RUNNING("Jogging", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    CYCLING("Cycling", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    SNOWBOARDING("Snowboarding", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER),
+    OTHERS("Others", ParameterType.TEMPERATURE, ParameterType.RAIN, ParameterType.WIND_POWER);
+
+    private String text;
+
+    private List<ParameterType> defaultParams;
+
+    SportTypes(String text, ParameterType... params) {
+        this.text = text;
+        if (params == null && params.length == 0) {
+            this.defaultParams = Arrays.asList();
+        }
+        this.defaultParams = Arrays.asList(params);
+    }
+}
